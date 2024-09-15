9f8d0c6feccfa854d6c473efe641b23d1db526df
Denys Dzhulai <denisjuly@gmail.com>
2024-09-15 10:50:43 -0600

    Added styling with old PC esthetics

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Example API fetch
 fetch(
   "https://api.open-meteo.com/v1/forecast?latitude=49.4445&longitude=32.0574&hourly=temperature_2m,precipitation_probability,precipitation,pressure_msl,visibility"
-) // Replace with your API endpoint
+)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    const temperature = data.hourly.temperature_2m[0];
+    const precipitation = data.hourly.precipitation[0];
+
+    document.querySelector(
+      "p"
+    ).textContent = `Temperature in Cherkasy: ${temperature}°C, Precipitation: ${precipitation}mm`;
   })
   .catch((error) => console.error("Error:", error));
