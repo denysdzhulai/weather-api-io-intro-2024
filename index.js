e05c4123993281df5e774c155852053e8afd44ca
Denys Dzhulai <denisjuly@gmail.com>
2024-09-18 20:40:37 -0600

    Added humidity.html and temperature.html, midified styles

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-fetch(
-  "https://api.open-meteo.com/v1/forecast?latitude=49.4445&longitude=32.0574&hourly=temperature_2m,precipitation_probability,precipitation,pressure_msl,visibility"
-)
+const API_URL =
+  "https://api.open-meteo.com/v1/forecast?latitude=45.4647&longitude=-98.4865&hourly=temperature_2m,relative_humidity_2m";
+
+fetch(API_URL)
   .then((response) => response.json())
   .then((data) => {
-    const temperature = data.hourly.temperature_2m[0];
-    const precipitation = data.hourly.precipitation[0];
+    const path = window.location.pathname;
 
-    document.querySelector(
-      "p"
-    ).textContent = `Temperature in Cherkasy: ${temperature}°C, Precipitation: ${precipitation}mm`;
+    if (path.includes("temperature.html")) {
+      displayTemperature(data);
+    } else if (path.includes("humidity.html")) {
+      displayRelativeHumidity(data);
+    } else {
+      displayHome(data);
+    }
   })
   .catch((error) => console.error("Error:", error));
+
+// Display temperature data
+function displayTemperature(data) {
+  const temperature = data.hourly.temperature_2m[0];
+  document.querySelector(
+    "p"
+  ).textContent = `Current Temperature: ${temperature}°C`;
+}
+
+// Display relative humidity data
+function displayRelativeHumidity(data) {
+  const humidity = data.hourly.relative_humidity_2m[0];
+  document.querySelector("p").textContent = `Current Humidity: ${humidity}%`;
+}
+
+// Display home page with summary
+function displayHome(data) {
+  const temperature = data.hourly.temperature_2m[0];
+  const humidity = data.hourly.relative_humidity_2m[0];
+  document.querySelector(
+    "p"
+  ).textContent = `Temperature: ${temperature}°C, Humidity: ${humidity}%`;
+}
